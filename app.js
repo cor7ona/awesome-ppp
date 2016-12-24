6bb41a523fbb0cd5b6418f6267e1f171497c93e8
zhanghua54 <zhanghua54@wanda.cn>
2016-12-24 13:28:07 +0800

    init

---
+++ app.js
@@ -0,0 +1,27 @@
+
+
+'use strict';
+const PORT = process.env.NODE_ENV === 'prod' ? 808: 3000;
+const express = require('express');
+const path = require('path');
+const favicon = require('static-favicon');
+const bodyParser = require('body-parser');
+const app = express();
+const server = app.listen(PORT, listenHandler);
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'html');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(favicon());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.all('*', (req, res) => {
+    res.render('index');
+});
+
+
+function listenHandler() {
+    console.log('Server started on port:' + PORT);
+}
